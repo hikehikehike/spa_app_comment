53eea351afa1eb224fc9f0e5b408b8b1d87b4468
Timur <timur.hike@gmail.com>
2023-02-21 01:50:56 +0200

    Add models comments

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class Comments(models.Model):
+    user_name = models.CharField(max_length=255)
+    mail = models.EmailField(max_length=255)
+    home_page = models.URLField(max_length=255, blank=True, null=True)
+    text = models.TextField()
+    modified = models.TimeField(auto_now_add=True)
+    parent = TreeForeignKey("Comments", on_delete=models.CASCADE,
+                            null=True, blank=True, related_name="children")
+
+    class MPTTMeta:
+        order_insertion_by = ["modified"]
+
+    def __str__(self):
+        return self.text
