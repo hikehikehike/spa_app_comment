556fe49866c4c7cc6a0ec52b945a51ace1cd94df
Timur <timur.hike@gmail.com>
2023-02-21 17:36:09 +0200

    Change model Comment

--- comment/models.py
+++ comment/models.py
@@ -1,18 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 from mptt.models import MPTTModel, TreeForeignKey
 
 
-class Comments(models.Model):
-    user_name = models.CharField(max_length=255)
-    mail = models.EmailField(max_length=255)
+class Comment(MPTTModel):
+    user_name = models.CharField(max_length=50)
+    parent = TreeForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
+    )
+    email = models.EmailField()
     home_page = models.URLField(max_length=255, blank=True, null=True)
     text = models.TextField()
-    modified = models.TimeField(auto_now_add=True)
-    parent = TreeForeignKey("Comments", on_delete=models.CASCADE,
-                            null=True, blank=True, related_name="children")
+    publish = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
 
     class MPTTMeta:
-        order_insertion_by = ["modified"]
+        order_insertion_by = ["-publish"]
 
     def __str__(self):
-        return self.text
+        return self.user_name
