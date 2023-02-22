30afe988883ce6123f25d4102fac0a30ab028d15
Timur <timur.hike@gmail.com>
2023-02-22 12:58:32 +0200

    Update secret_key

--- spa_app_comment/settings.py
+++ spa_app_comment/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "^2_(7xa(=9_h!@#jwv4=^hky2)^^tr-p7a3dz&5ra0zgm3om*7")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
